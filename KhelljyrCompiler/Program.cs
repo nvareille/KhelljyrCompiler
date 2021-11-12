d86f165d87b7783170868650c0559f48be5a0a23
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2021-11-12 10:38:07 +0100

    Add project files.

---
+++ KhelljyrCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KhelljyrCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
